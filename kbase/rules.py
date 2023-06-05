0cf6151ef3bd9a934941248aa235e720b3ad6591
zianttt <ziant0927@gmail.com>
2023-06-05 15:10:19 +0800

    update user cf

--- kbase/rules.py
+++ kbase/rules.py
@@ -1,79 +1,80 @@
 # Define the rules inside a rule-based expert system
 class Rule:
-    def __init__(self, expert_cf, apply_rule):
+    def __init__(self, user_cf, expert_cf, apply_rule):
+        self.user_cf = user_cf
         self.expert_cf = expert_cf
         self.apply_rule = apply_rule
 
 def obesity_rules():
     return [
-        Rule(1.0, lambda patient: 25 <= patient.data['bmi'] >= 30),
+        Rule(1.0, 1.0, lambda patient: 25 <= patient.data['bmi'] >= 30),
     ]
 
 def gestational_diabetes_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['1_hour_plasma_glucose_level'] >= 180),
-        Rule(0.8, lambda patient: patient.data['gestational_diabetes_history']),
-        Rule(0.8, lambda patient: patient.data['bmi'] >= 30),
+        Rule(1.0, 0.9, lambda patient: patient.data['1_hour_plasma_glucose_level'] >= 180),
+        Rule(1.0, 0.75, lambda patient: patient.data['gestational_diabetes_history']),
+        Rule(1.0, 1.0, lambda patient: patient.data['bmi'] >= 30),
     ]
 
 def hypertension_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['systolic_blood_pressure'] >= 140 or patient.data['diastolic_blood_pressure'] >= 90),
+        Rule(1.0, 0.95, lambda patient: patient.data['systolic_blood_pressure'] >= 140 or patient.data['diastolic_blood_pressure'] >= 90),
         # Preemclampia symptoms
-        Rule(0.8, lambda patient: patient.data['urine_protein_level'] > 300),
-        Rule(0.8, lambda patient: patient.data['blurred_vision']),
+        Rule(1.0, 0.7, lambda patient: patient.data['urine_protein_level'] > 300),
+        Rule(0.8, 0.55, lambda patient: patient.data['blurred_vision']),
     ]
 
 def anaemia_rules():
     # translate the following rules into python code
     return [
-        Rule(0.8, lambda patient: patient.data['haemoglobin_level'] < 11),
+        Rule(1.0, 0.95, lambda patient: patient.data['haemoglobin_level'] < 11),
     ]
 
 def rickets_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['calcium_level'] < 8.5),
-        Rule(0.8, lambda patient: patient.data['phosphate_level'] < 2.5),
+        Rule(1.0, 0.8, lambda patient: patient.data['calcium_level'] < 8.5),
+        Rule(1.0, 0.8, lambda patient: patient.data['phosphate_level'] < 2.5),
     ]
 
 def kidney_diseases_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['urine_protein_level'] > 300),
-        Rule(0.8, lambda patient: patient.data['blood_creatinine_level'] > 0.85),
-        Rule(0.8, lambda patient: patient.data['weeks_pregnant'] < 12 and patient.data['blood_urea_nitrogen_level'] > 12),
-        Rule(0.8, lambda patient: patient.data['weeks_pregnant'] > 12 and patient.data['weeks_pregnant'] < 24 and patient.data['blood_urea_nitrogen_level'] > 13),
-        Rule(0.8, lambda patient: patient.data['weeks_pregnant'] > 24 and patient.data['blood_urea_nitrogen_level'] > 11),
-        Rule(0.8, lambda patient: patient.data['hypertension']),
-        Rule(0.8, lambda patient: patient.data['fatigue']),
+        Rule(1.0, 0.8, lambda patient: patient.data['urine_protein_level'] > 300),
+        Rule(1.0, 0.8, lambda patient: patient.data['blood_creatinine_level'] > 0.85),
+        Rule(1.0, 0.8, lambda patient: patient.data['weeks_pregnant'] < 12 and patient.data['blood_urea_nitrogen_level'] > 12),
+        Rule(1.0, 0.8, lambda patient: patient.data['weeks_pregnant'] > 12 and patient.data['weeks_pregnant'] < 24 and patient.data['blood_urea_nitrogen_level'] > 13),
+        Rule(1.0, 0.8, lambda patient: patient.data['weeks_pregnant'] > 24 and patient.data['blood_urea_nitrogen_level'] > 11),
+        Rule(1.0, 0.8, lambda patient: patient.data['hypertension']),
+        Rule(1.0, 0.8, lambda patient: patient.data['fatigue']),
     ]
 
 def scurvy_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['fatigue']),
-        Rule(0.8, lambda patient: patient.data['gingivitis']),
-        Rule(0.8, lambda patient: patient.data['vitamin_c_level'] < 0.02),
+        Rule(1.0, 0.8, lambda patient: patient.data['fatigue']),
+        Rule(1.0, 0.8, lambda patient: patient.data['gingivitis']),
+        Rule(1.0, 0.8, lambda patient: patient.data['vitamin_c_level'] < 0.02),
     ]
 
 def heart_disease_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['hypertension']),
-        Rule(0.8, lambda patient: patient.data['weeks_pregnant'] < 12 and (patient.data['ldlc_level'] > 3.27 or patient.data['hdlc_level'] < 1.23)),
-        Rule(0.8, lambda patient: patient.data['weeks_pregnant'] > 12 and (patient.data['ldlc_level'] > 4.83 or patient.data['hdlc_level'] < 1.41)),
-        Rule(0.8, lambda patient: patient.data['bmi'] > 30),
-        Rule(0.8, lambda patient: patient.data['family_history_heart_disease']),
-        Rule(0.8, lambda patient: patient.data['gestational_diabetes']),
-        Rule(0.8, lambda patient: patient.data['chest_pain'] or patient.data['fatigue']),
+        Rule(0.95, 0.75, lambda patient: patient.data['hypertension']),
+        Rule(1.0, 0.75, lambda patient: patient.data['weeks_pregnant'] < 12 and (patient.data['ldlc_level'] > 3.27 or patient.data['hdlc_level'] < 1.23)),
+        Rule(1.0, 0.75, lambda patient: patient.data['weeks_pregnant'] > 12 and (patient.data['ldlc_level'] > 4.83 or patient.data['hdlc_level'] < 1.41)),
+        Rule(1.0, 0.7, lambda patient: 25 <= patient.data['bmi'] >= 30),
+        Rule(0.95, 0.75, lambda patient: patient.data['family_history_heart_disease']),
+        Rule(0.95, 0.55, lambda patient: patient.data['gestational_diabetes']),
+        Rule(0.8, 0.7, lambda patient: patient.data['chest_pain'] or patient.data['fatigue']),
     ]
 
 def eye_disease_rules():
     return [
-        Rule(0.8, lambda patient: patient.data['blurred_vision']),
-        Rule(0.8, lambda patient: patient.data['floating_spots']),
+        Rule(0.8, 0.55, lambda patient: patient.data['blurred_vision']),
+        Rule(0.8, 0.55, lambda patient: patient.data['floating_spots']),
     ]
     
-def compute_CF(user_CF, rule, patient):
+def compute_CF(rule, patient):
     if rule.apply_rule(patient):
-        return user_CF * rule.expert_cf
+        return rule.user_CF * rule.expert_cf
     else:
         return 0
         
@@ -100,12 +101,11 @@ def evaluate_all_rules(patient):
 
     diagnosis_results = {}
 
-    user_CF = 0.7
     threshold = 0.95
 
     for disease in check_sequence:
         rules = diseases[disease]
-        cfs = [compute_CF(user_CF, rule, patient) for rule in rules]
+        cfs = [compute_CF(rule, patient) for rule in rules]
         cf_combined = combine_CFs(cfs)
         confidence_percentage = cf_combined * 100
 
